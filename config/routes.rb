8535e1696f5dc9c8ab46b4777df6aaa8c6f66f0f
Alexis Bernard <alexis@basesecrete.com>
2021-03-05 12:24:38 +0100

    Handle dots in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActiveAnalytics::Engine.routes.draw do
-  get "/:site", to: "page_views#show", as: :page_view
-  get "/:site/*page", to: "page_views#show"
+  get "/:site", to: "page_views#show", as: :page_view, constraints: {site: /[^\/]+/}
+  get "/:site/*page", to: "page_views#show", constraints: {site: /[^\/]+/}
   root to: "page_views#index"
 end
