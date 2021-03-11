4e8af5a17b93e526808ea90111b625b4c34b924f
Alexis Bernard <alexis@basesecrete.com>
2021-03-11 18:23:55 +0100

    Add index for pages

--- app/controllers/active_analytics/pages_controller.rb
+++ app/controllers/active_analytics/pages_controller.rb
@@ -4,6 +4,12 @@ module ActiveAnalytics
   class PagesController < ApplicationController
     include PagesHelper
 
+    def index
+      scope = ViewsPerDay.where(site: params[:site]).after(30.days.ago)
+      @views_per_day = scope.order_by_date.group_by_date
+      @pages = scope.top(100).group_by_page
+    end
+
     def show
       scope = ViewsPerDay.where(site: params[:site], page: page_from_params).after(30.days.ago)
       @views_per_day = scope.order_by_date.group_by_date
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 ActiveAnalytics::Engine.routes.draw do
   get "/:site", to: "sites#show", as: :site, constraints: {site: /[^\/]+/}
 
+  # Referrers
   get "/:site/referrers", to: "referrers#index", constraints: {site: /[^\/]+/}, as: :referrers
   get "/:site/referrers/:referrer", to: "referrers#show", constraints: {site: /[^\/]+/, referrer: /[^\/]+/}, as: :referrer
 
+  # Pages
+  get "/:site/pages", to: "pages#index", constraints: {site: /[^\/]+/}, as: :pages
   get "/:site/*page", to: "pages#show", as: :page, constraints: {site: /[^\/]+/}
+
   root to: "sites#index", as: :active_analytics
 end
