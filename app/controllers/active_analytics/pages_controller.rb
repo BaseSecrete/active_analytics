71d560b07f68dcc2c3b5820b424b8900e700ce1a
Alexis Bernard <alexis@basesecrete.com>
2021-03-11 18:00:50 +0100

    Handle root page in routes

--- app/controllers/active_analytics/pages_controller.rb
+++ app/controllers/active_analytics/pages_controller.rb
@@ -2,13 +2,15 @@ require_dependency "active_analytics/application_controller"
 
 module ActiveAnalytics
   class PagesController < ApplicationController
+    include PagesHelper
+
     def show
-      scope = ViewsPerDay.where(site: params[:site], page: "/" + params[:page]).after(30.days.ago)
+      scope = ViewsPerDay.where(site: params[:site], page: page_from_params).after(30.days.ago)
       @views_per_day = scope.order_by_date.group_by_date
       @referrers = scope.top.group_by_referrer_site
 
-      @next_pages = ViewsPerDay.where(referrer_host: params[:site], referrer_path: "/" + params[:page]).top.group_by_page
-      @previous_pages = ViewsPerDay.where(site: params[:site], page: "/" + params[:page]).top.group_by_referrer_page
+      @next_pages = ViewsPerDay.where(referrer_host: params[:site], referrer_path: page_from_params).top.group_by_page
+      @previous_pages = ViewsPerDay.where(site: params[:site], page: page_from_params).top.group_by_referrer_page
     end
   end
 end
--- app/helpers/active_analytics/pages_helper.rb
+++ app/helpers/active_analytics/pages_helper.rb
@@ -1,4 +1,15 @@
 module ActiveAnalytics
   module PagesHelper
+    def page_to_params(name)
+      name == "/" ? "index" : name[1..-1]
+    end
+
+    def page_from_params
+      if params[:page] == "index"
+        "/"
+      elsif params[:page].present?
+        "/#{params[:page]}"
+      end
+    end
   end
 end
