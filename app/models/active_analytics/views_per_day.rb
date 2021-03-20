d2948ec9e86af2a8bc3565ceac5069852e7cb9fa
Alexis Bernard <alexis@basesecrete.com>
2021-03-20 11:23:03 +0100

    Add histogram

--- app/models/active_analytics/views_per_day.rb
+++ app/models/active_analytics/views_per_day.rb
@@ -32,6 +32,30 @@ module ActiveAnalytics
       end
     end
 
+    class Histogram
+      attr_reader :bars
+
+      def initialize(scope)
+        @bars = scope.map { |day| Bar.new(day.day, day.total, self) }
+      end
+
+      def max_value
+        @max_total ||= bars.map(&:value).max
+      end
+
+      class Bar
+        attr_reader :label, :value, :histogram
+
+        def initialize(label, value, histogram)
+          @label, @value,@histogram = label, value, histogram
+        end
+
+        def height
+          (value.to_f / histogram.max_value).round(2)
+        end
+      end
+    end
+
     def self.group_by_site
       group(:site).pluck("site, SUM(total)").map do |row|
         Site.new(row[0], row[1])
