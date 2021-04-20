bba0fcb4d0c50256d16b74cce42e421ef16083de
Alexis Bernard <alexis@basesecrete.com>
2021-04-20 15:38:35 +0200

    Add missing date filter for referrers

--- app/controllers/active_analytics/referrers_controller.rb
+++ app/controllers/active_analytics/referrers_controller.rb
@@ -5,13 +5,13 @@ module ActiveAnalytics
     before_action :require_date_range
 
     def index
-      scope = ViewsPerDay.where(site: params[:site])
+      scope = ViewsPerDay.where(site: params[:site]).between_dates(params[:from], params[:to])
       @referrers = scope.top(100).group_by_referrer_site
       @histogram = ViewsPerDay::Histogram.new(scope.order_by_date.group_by_date)
     end
 
     def show
-      scope = ViewsPerDay.where(site: params[:site], referrer_host: params[:referrer])
+      scope = ViewsPerDay.where(site: params[:site], referrer_host: params[:referrer]).between_dates(params[:from], params[:to])
       @histogram = ViewsPerDay::Histogram.new(scope.order_by_date.group_by_date)
       @previous_pages = scope.top(100).group_by_referrer_page
       @next_pages = scope.top(100).group_by_page
