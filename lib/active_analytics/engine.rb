13e089227771c2d30b61b080b2b7314a05ea6817
Alexis Bernard <alexis@basesecrete.com>
2021-06-26 14:48:47 +0200

    Specifiy assets to precompile
    Thanks to Wasif Hossain for the suggestion

--- lib/active_analytics/engine.rb
+++ lib/active_analytics/engine.rb
@@ -1,5 +1,9 @@
 module ActiveAnalytics
   class Engine < ::Rails::Engine
     isolate_namespace ActiveAnalytics
+
+    initializer "active_analytics.assets.precompile" do |app|
+      app.config.assets.precompile += %w[active_analytics/application.js active_analytics/application.css]
+    end
   end
 end
