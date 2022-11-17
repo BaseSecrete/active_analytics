c053e0ad01f7f26c914ecc6f8f393154156e29c3
Alexis Bernard <alexis@basesecrete.com>
2022-11-17 06:13:07 +0100

    Prevent from crashing when application does not use asset pipeline

--- lib/active_analytics/engine.rb
+++ lib/active_analytics/engine.rb
@@ -3,7 +3,9 @@ module ActiveAnalytics
     isolate_namespace ActiveAnalytics
 
     initializer "active_analytics.assets.precompile" do |app|
-      app.config.assets.precompile += %w[active_analytics/application.js active_analytics/application.css]
+      if app.config.respond_to?(:assets)
+        app.config.assets.precompile += %w[active_analytics/application.js active_analytics/application.css]
+      end
     end
   end
 end
