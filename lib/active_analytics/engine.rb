2df4fdbea80d8ff68ffda5d7df92301ee1fbcb59
Alexis Bernard <alexis@basesecrete.com>
2023-04-22 08:52:15 +0200

    Remove last reference to asset pipeline

--- lib/active_analytics/engine.rb
+++ lib/active_analytics/engine.rb
@@ -1,11 +1,5 @@
 module ActiveAnalytics
   class Engine < ::Rails::Engine
     isolate_namespace ActiveAnalytics
-
-    initializer "active_analytics.assets.precompile" do |app|
-      if app.config.respond_to?(:assets)
-        app.config.assets.precompile += %w[active_analytics/application.js active_analytics/application.css]
-      end
-    end
   end
 end
