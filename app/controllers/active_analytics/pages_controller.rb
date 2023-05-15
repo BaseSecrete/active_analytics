997f3fffed1511e41731c64246d36db8c9cff5d5
Alexis Bernard <alexis@basesecrete.com>
2023-05-15 09:04:01 +0200

    Fix comparison

--- app/controllers/active_analytics/pages_controller.rb
+++ app/controllers/active_analytics/pages_controller.rb
@@ -14,8 +14,9 @@ module ActiveAnalytics
 
     def show
       page_scope = current_views_per_days.where(page: page_from_params)
+      previous_page_scope = previous_views_per_days.where(page: page_from_params)
       @histogram = ViewsPerDay::Histogram.new(page_scope.order_by_date.group_by_date, from_date, to_date)
-      @previous_histogram = ViewsPerDay::Histogram.new(previous_views_per_days.order_by_date.group_by_date, previous_from_date, previous_to_date)
+      @previous_histogram = ViewsPerDay::Histogram.new(previous_page_scope.order_by_date.group_by_date, previous_from_date, previous_to_date)
       @next_pages = current_views_per_days.where(referrer_host: params[:site], referrer_path: page_from_params).top(100).group_by_page
       @previous_pages = page_scope.top(100).group_by_referrer_page
     end
