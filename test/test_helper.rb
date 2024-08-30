25d9429cc49351cd3bc89a9d04895a3369a2b74c
Alexis Bernard <alexis@basesecrete.com>
2024-08-30 16:38:47 +0200

    Update dependencies
    Fix deprecated fixture_path.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,11 +6,7 @@ ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/mi
 ActiveRecord::Migrator.migrations_paths << File.expand_path('../db/migrate', __dir__)
 require "rails/test_help"
 
-
-# Load fixtures from the engine
-if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
-  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
-  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
-  ActiveSupport::TestCase.fixtures :all
-end
+ActiveSupport::TestCase.fixture_paths << File.expand_path("fixtures", __dir__)
+ActionDispatch::IntegrationTest.fixture_paths << File.expand_path("fixtures", __dir__)
+ActiveSupport::TestCase.fixture_paths << File.expand_path("fixtures/files", __dir__)
+ActiveSupport::TestCase.fixtures :all
