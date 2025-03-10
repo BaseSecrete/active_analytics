aa40ad4937e38d0d5c646da0c52dab896938d534
Carlos '0rientd' Henrique <10491532+0rientd@users.noreply.github.com>
2025-03-10 06:18:32 -0300

    Shortened index name in migration to prevent argument error (index name too long) (#15)

--- db/migrate/20210303094108_create_active_analytics_views_per_days.rb
+++ db/migrate/20210303094108_create_active_analytics_views_per_days.rb
@@ -10,7 +10,8 @@ class CreateActiveAnalyticsViewsPerDays < ActiveRecord::Migration[5.2]
       t.timestamps
     end
     add_index :active_analytics_views_per_days, [:date, :site, :page]
-    add_index :active_analytics_views_per_days, [:date, :site, :referrer_host, :referrer_path]
+    add_index :active_analytics_views_per_days, [:date, :site, :referrer_host, :referrer_path],
+              name: 'index_views_per_days_on_date_site_referrer_host_referrer_path'
   end
 
   def down
